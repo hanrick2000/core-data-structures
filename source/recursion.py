a3f9cae78e11fc062f16edba7ad325dba3a8c206
Tia King <tiajaneeking@gmail.com>
2018-02-27 23:23:01 -0800

    implemented factorial iteratively, passes all tests

--- source/recursion.py
+++ source/recursion.py
@@ -8,13 +8,17 @@ def factorial(n):
         raise ValueError('factorial is undefined for n = {}'.format(n))
     # implement factorial_iterative and factorial_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    # return factorial_iterative(n)
-    return factorial_recursive(n)
+    return factorial_iterative(n)
+#   return factorial_recursive(n)
 
 
 def factorial_iterative(n):
-    # TODO: implement the factorial function iteratively here
-    pass
+    answer = 1
+    #iterate through all numbers up to n
+    for i in range(1, n+1):
+        #answer is each number multiplied together
+        answer *= i
+    return answer
     # once implemented, change factorial (above) to call factorial_iterative
     # to verify that your iterative implementation passes all tests
 
